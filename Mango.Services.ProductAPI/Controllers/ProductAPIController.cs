21270a7a303b6af814120d8380ab35c260e2779c
Raed Altaki <raed.simon@gmail.com>
2023-12-09 14:17:54 -0500

    Added the products to the home page and implemented the product details page

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -12,7 +12,7 @@ namespace Mango.Services.ProductAPI.Controllers
 {
     [Route("api/product")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class ProductAPIController : ControllerBase
     {
         private readonly AppDbContext _db;
