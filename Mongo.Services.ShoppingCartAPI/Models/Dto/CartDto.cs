9d2f2340dc3e61515c9594d6dbaa86dad37b8c86
Raed Altaki <raed.simon@gmail.com>
2023-12-09 21:25:48 -0500

    Initialized the Cart project and created the Models and DTOs

---
+++ Mongo.Services.ShoppingCartAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,10 @@
+﻿using Mongo.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDto
+    {
+        public CartHeaderDto CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+    }
+}
