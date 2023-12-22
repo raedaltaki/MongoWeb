115d87449ce793ae0da9498c1c2b22d142dda372
Raed Altaki <raed.simon@gmail.com>
2023-12-21 23:42:26 -0500

    Added cart to the web project and fix applying the coupon bug

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,66 +1,110 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using IdentityModel;
 using Mango.Web.Models;
 using Mango.Web.Service.IService;
-using Newtonsoft.Json;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
-namespace Mango.Web.Controllers;
-
-public class HomeController : Controller
+namespace Mango.Web.Controllers
 {
-    private readonly IProductService _productService;
-    public HomeController(IProductService productService)
+    public class HomeController : Controller
     {
-        _productService = productService;
-    }
-
-    public async Task<IActionResult> Index()
-    {
-        List<ProductDto>? list = new();
-
-        ResponseDto? response = await _productService.GetAllProductsAsync();
-
-        if (response != null && response.IsSuccess)
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+        public HomeController(IProductService productService, ICartService cartService)
         {
-            list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
-        }
-        else
-        {
-            TempData["error"] = response?.Message;
+            _productService = productService;
+            _cartService = cartService;
         }
 
-        return View(list);
-    }
 
-    [Authorize]
-    public async Task<IActionResult> ProductDetails(int productId)
-    {
-        ProductDto model = new();
-
-        ResponseDto? response = await _productService.GetProductByIdAsync(productId);
-
-        if (response != null && response.IsSuccess)
+        public async Task<IActionResult> Index()
         {
-            model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            List<ProductDto>? list = new();
+
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(list);
         }
-        else
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
         {
-            TempData["error"] = response?.Message;
+            ProductDto? model = new();
+
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(model);
         }
 
-        return View(model);
-    }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        [Authorize]
+        [HttpPost]
+        [ActionName("ProductDetails")]
+        public async Task<IActionResult> ProductDetails(ProductDto productDto)
+        {
+            CartDto cartDto = new CartDto()
+            {
+                CartHeader = new CartHeaderDto
+                {
+                    UserId = User.Claims.Where(u => u.Type == JwtClaimTypes.Subject)?.FirstOrDefault()?.Value
+                }
+            };
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            CartDetailsDto cartDetails = new CartDetailsDto()
+            {
+                Count = productDto.Count,
+                ProductId = productDto.ProductId,
+            };
+
+            List<CartDetailsDto> cartDetailsDtos = new() { cartDetails };
+            cartDto.CartDetails = cartDetailsDtos;
+
+            ResponseDto? response = await _cartService.UpsertCartAsync(cartDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Item has been added to the Shopping Cart";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDto);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
-
--- Mongo.Services.ShoppingCartAPI/Program.cs
+++ Mongo.Services.ShoppingCartAPI/Program.cs
@@ -7,6 +7,7 @@ using Mango.Services.ShoppingCartAPI;
 using Mongo.Services.ShoppingCartAPI.Extensions;
 using Mango.Services.ShoppingCartAPI.Service;
 using Mango.Services.ShoppingCartAPI.Service.IService;
+using Mango.Services.ShoppingCartAPI.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,9 +24,15 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<BackendApiAuthenticationHttpClientHandler>();
+builder.Services.AddHttpClient("Product", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:ProductAPI"]))
+    .AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
+builder.Services.AddHttpClient("Coupon", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:CouponAPI"]))
+    .AddHttpMessageHandler<BackendApiAuthenticationHttpClientHandler>();
 
-builder.Services.AddHttpClient("Product", u => u.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
-builder.Services.AddHttpClient("Coupon", u => u.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
